70d3eb3350e995d12567d9cc6634b93018b5c7a2
harshvardhan jha <harshjha038@gmail.com>
2021-08-23 19:34:03 +0530

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,18 +14,19 @@ function Footer() {
                <div className="footerimage">
                   <img src="https://www.arcadeagroup.com/a-footer.svg"></img>
                </div>
+            </Col>
+            <Col md={6}>
                <div className="footerform">
                   <form action="https://arcadeagroup.us6.list-manage.com/subscribe/post" method="POST">
                      <input type="hidden" name="u" value="526cde56bb3f37173cfaa0a6b"/><input type="hidden" name="id" value="256e73495e"/>
-                     <h3 className="text-white ">The Arcadea Brief</h3>
+                     <h2 className="text-white ">The Arcadea Brief</h2>
                      <p className="text-white ">Join the Arcadea Brief, where we share <br/> our latest ideas and lessons learned, <br/> straight to your inbox.</p>
                      <div className="news-subscription">
                         <input type="email" className="p-2 border border-white bg-dark" name="EMAIL" placeholder="Your email" required=""/><a className="readmore" href="/" type="submit">Sign&nbsp;up</a>
                      </div>
                   </form>
                </div>
-            </Col>
-            
+            </Col>  
          </Row>
          <Row className="mt-5 pb-5">
             <Col className="d-flex justify-content-center pt-3">
